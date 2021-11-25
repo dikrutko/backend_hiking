9b05fdae6aa688e6032d4515d4e259c3e8761846
dikrutko <82881342+dikrutko@users.noreply.github.com>
2021-11-25 11:18:24 +0700

    changes url

--- models/base_model.py
+++ models/base_model.py
@@ -1,10 +1,18 @@
 import peewee
 from os import environ
-from playhouse.cockroachdb import CockroachDatabase
+import urllib.parse
 
 
 if 'DATABASE_URL' in environ:
-    db = CockroachDatabase(environ['DATABASE_URL'])
+    urllib.parse.uses_netloc.append('postgres')
+    url = urllib.parse.urlparse(environ['DATABASE_URL'])
+    db = peewee.PostgresqlDatabase(
+        database=url.path[1:],
+        user=url.username,
+        password=url.password,
+        host=url.hostname,
+        port=url.port
+    )
 else:
     db = peewee.SqliteDatabase('people.db')
 
