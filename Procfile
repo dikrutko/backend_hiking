8e1b145abac09c0ecf25d9f254457e1ba6456859
dikrutko <82881342+dikrutko@users.noreply.github.com>
2021-11-25 10:57:03 +0700

    sets heruko

---
+++ Procfile
@@ -0,0 +1,1 @@
+web: guniron main:app
--- models/base_model.py
+++ models/base_model.py
@@ -1,7 +1,12 @@
 import peewee
+from os import environ
+from playhouse.cockroachdb import CockroachDatabase
 
 
-db = peewee.SqliteDatabase('people.db')
+if 'DATABASE_URL' in environ:
+    db = CockroachDatabase(environ['DATABASE_URL'])
+else:
+    db = peewee.SqliteDatabase('people.db')
 
 
 class BaseModel(peewee.Model):
