5fa9f3bf24dfea8aad551b18c55c303fef939f0b
dikrutko <82881342+dikrutko@users.noreply.github.com>
2021-11-25 21:47:42 +0700

    add def for new tables

--- src/main.py
+++ src/main.py
@@ -46,26 +46,65 @@ def del_areas(pk):
 
 @app.route('/routes', methods=['GET'])
 def get_routes():
-    """Получение всех мрашрутов"""
+    """Получение всех маршрутов"""
     return convert_all_object_to_json(models.Route, exclude=[models.Route.area])
 
 @app.route('/routes/<pk>')
-def get_detail_route(pk):
+def get_detail_routes(pk):
     """Получение детальной информации о маршруте с id = Pk"""
     return convert_all_object_to_json(
         models.Route.select().where(models.Route.id == int(pk))) 
 
 @app.route('/routes', methods=['POST'])
-def add_areas():
-    """Создание нового района из json"""
+def add_routes():
+    """Создание нового маршрута из json"""
     return create_object_from_json(models.Route, request.json)
 
 @app.route('/routes/<pk>', methods=['DELETE'])
-def del_areas(pk):
+def del_routes(pk):
     models.Route.delete().where(models.Route.id == int(pk)).execute()
-    """Удаление района из json"""
+    """Удаление маршрута из json"""
     return jsonify({'status': 'deleted'})
 
+
+@app.route('/objects', methods=['GET'])
+def get_objects():
+    """Получение всех маршрутов"""
+    return convert_all_object_to_json(models.Object, exclude=[models.Object.area])########## route?
+
+@app.route('/objects/<pk>')
+def get_detail_objects(pk):
+    """Получение детальной информации о маршруте с id = Pk"""
+    return convert_all_object_to_json(
+        models.Object.select().where(models.Object.id == int(pk))) 
+
+@app.route('/objects', methods=['POST'])
+def add_objects():
+    """Создание нового маршрута из json"""
+    return create_object_from_json(models.Object, request.json)
+
+@app.route('/objects/<pk>', methods=['DELETE'])
+def del_objects(pk):
+    models.Object.delete().where(models.Object.id == int(pk)).execute()
+    """Удаление маршрута из json"""
+    return jsonify({'status': 'deleted'})
+
+
+@app.route('/team', methods=['GET'])
+def get_team():
+    """Получение всех людей из командв"""
+    return convert_all_object_to_json(models.Team)
+
+@app.route('/team', methods=['POST'])
+def add_team():
+    """Добавление нового человека в команду из json"""
+    return create_object_from_json(models.Team, request.json)
+
+@app.route('/team/<pk>', methods=['DELETE'])
+def del_team(pk):
+    models.Team.delete().where(models.Team.id == int(pk)).execute()
+    """Удаление человека из команды из json"""
+    return jsonify({'status': 'deleted'})
 
 
 if __name__ == '__main__':
