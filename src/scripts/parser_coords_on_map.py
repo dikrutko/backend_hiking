95da06dfa76a8ca02cd3b6b4f483b63fc6563967
dikrutko <82881342+dikrutko@users.noreply.github.com>
2021-12-15 22:50:03 +0700

    correct

--- src/scripts/parser_coords_on_map.py
+++ src/scripts/parser_coords_on_map.py
@@ -10,6 +10,5 @@ def pars_coords(name):
         if child.nodeType == child.ELEMENT_NODE:
             latValue = round(float(child.getAttribute("lat")),6)
             lonValue = round(float(child.getAttribute("lon")),6)
-
             result += f'geoPoints.add(new GeoPoint({latValue}), ({lonValue}));<br>'
     return result
