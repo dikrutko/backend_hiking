dd76e8f32f19a87b471a8c374485215f33ef40cc
dikrutko <82881342+dikrutko@users.noreply.github.com>
2021-12-22 15:24:36 +0700

    edit

--- src/plugins/user/models.py
+++ src/plugins/user/models.py
@@ -4,7 +4,7 @@ from plugins.core.base_model import BaseModel
 
 class User(BaseModel):
     name = peewee.CharField(index=True)
-    date = peewee.DateField(index=True)
+    birthday = peewee.DateField(index=True)
     phone = peewee.CharField(index=True)
     email = peewee.CharField(index=True)
     password = peewee.CharField()
