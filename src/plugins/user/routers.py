bc89c1b68defacd516bc9495678f32eca8411d11
dikrutko <82881342+dikrutko@users.noreply.github.com>
2021-12-29 14:17:15 +0700

    .

--- src/plugins/user/routers.py
+++ src/plugins/user/routers.py
@@ -21,7 +21,7 @@ def send_code(user):
     text = f'Пожалуйста, подтвердите вашу почту: {url}/activations/{code}'
     
     send = post(
-        "https://api.mailgun.net/v3/.mailgun.org/messages",
+        f'https://api.mailgun.net/v3/{getenv("MAILGUN_MAIL").split("@")[1]}/messages',
         auth=("api", getenv('MAILGUN_API_KEY')),
         data={"from": f"Excited User <{getenv('MAILGUN_MAIL')}>",
               "to": [user.email],
