231d575b38b358906a77a510d8fb77431b68be3c
Anton Abramov <antonabramov2000@gmail.com>
2022-03-18 21:13:20 +0700

    Update models.py

--- src/plugins/area/models.py
+++ src/plugins/area/models.py
@@ -6,8 +6,15 @@ class Point(BaseModel):
     latitude = peewee.DoubleField()
     longitude = peewee.DoubleField()
 
+    
 class Area(BaseModel):
     name = peewee.CharField(index=True)
     description = peewee.TextField()
     point = peewee.ForeignKeyField(Point, backref='area')
     picture = peewee.TextField()
+
+    
+__all__ = [
+    Point,
+    Area,
+]
