3292444d7a17156a26fa71c08ffd72e0ff8a25bc
dikrutko <82881342+dikrutko@users.noreply.github.com>
2022-03-19 10:26:38 +0700

    .

--- src/plugins/area/models.py
+++ src/plugins/area/models.py
@@ -6,11 +6,13 @@ class Point(BaseModel):
     latitude = peewee.DoubleField()
     longitude = peewee.DoubleField()
 
+
 class Area(BaseModel):
     name = peewee.CharField(index=True)
     description = peewee.TextField()
     point = peewee.ForeignKeyField(Point, backref='area')
     picture = peewee.TextField()
+
 
 __all__ = [
     Point,
