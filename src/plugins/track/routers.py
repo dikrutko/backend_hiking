fad35c59cb6e8054e65006070d02c9b91a978284
dikrutko <82881342+dikrutko@users.noreply.github.com>
2022-03-22 16:39:00 +0700

    import vocab

--- src/plugins/track/routers.py
+++ src/plugins/track/routers.py
@@ -3,6 +3,7 @@ from plugins.core.plugin_manager import PluginManager
 from plugins.track.models import Track
 from utils import convert_all_object_to_json, create_object_from_json
 from scripts.new_track import Deikstra, lenforfway
+from scripts.vocab_base import vocab
 
 manager = PluginManager(None)
 
