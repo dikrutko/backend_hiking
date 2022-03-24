6a7007aa0ccdd60bc0219905fac9b4ea6e9c7aac
dikrutko <82881342+dikrutko@users.noreply.github.com>
2022-03-24 12:35:24 +0700

    name

--- src/plugins/news/routers.py
+++ src/plugins/news/routers.py
@@ -56,9 +56,9 @@ def load_news_from_vk():
         # Достаем название
         text_name = text.split('\n')[0]
         #name = re.findall(r'([А-Я]{2,}\s){1,}[А-Я]{2,}', text_name)
-        text_name = re.findall(r'([А-Я]{2,}\s)', text_name)
-        text_name1 = re.findall(r'([А-Я]{2,}\s[А-Я]{2,})', text_name)
-        text_name2 = re.findall(r'[А-Я]{2,}\s[А-Я]{2,}\s[А-Я]{2,}', text_name)
+        text_name = re.findall(r'([А-Я]{2,}\s)', str(text_name))
+        text_name1 = re.findall(r'([А-Я]{2,}\s[А-Я]{2,})', str(text_name))
+        text_name2 = re.findall(r'[А-Я]{2,}\s[А-Я]{2,}\s[А-Я]{2,}', str(text_name))
         if (text_name != ' ' or text_name1 != ' ' or text_name2 != ' '):
             if (text_name2 != ''):
                 name = text_name2
