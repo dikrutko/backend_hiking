0ce0cb8c8b5e386ac7d9c049983bb67d7b1835bf
dikrutko <82881342+dikrutko@users.noreply.github.com>
2022-03-24 17:56:46 +0700

    point from area del

--- src/plugins/area/models.py
+++ src/plugins/area/models.py
@@ -10,7 +10,7 @@ from plugins.core.base_model import BaseModel
 class Area(BaseModel):
     name = peewee.CharField(index=True)
     description = peewee.TextField()
-    point = peewee.ForeignKeyField(Point, backref='area')
+    #point = peewee.ForeignKeyField(Point, backref='area')
     latitude = peewee.DoubleField()
     longitude = peewee.DoubleField()
     picture = peewee.TextField()
