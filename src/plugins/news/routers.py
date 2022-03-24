60386e0cb005a6ea0f29a334229c79d962146357
dikrutko <82881342+dikrutko@users.noreply.github.com>
2022-03-24 20:28:47 +0700

    /

--- src/plugins/news/routers.py
+++ src/plugins/news/routers.py
@@ -117,10 +117,10 @@ def load_news_from_vk():
         lenght_time_event1 = re.findall(r'[пП]родолжительность\sвосхождения\W{1,3}\d+', text)
         if lenght_time_event1 != '':
             len_time_num = re.findall(r'\d+', str(lenght_time_event1))
-            lenght_time = str(len_time_num)
+            lenght_time = len_time_num[1]
         if lenght_time_event != '':
             len_time_num = re.findall(r'\d+', str(lenght_time_event))
-            lenght_time = str(len_time_num)
+            lenght_time = len_time_num[1]
         
 
         # Ссылку на регистриацию
