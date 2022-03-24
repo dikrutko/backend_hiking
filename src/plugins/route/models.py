ad30ed66253f8be60dc86afca78a5beedfa3d56f
dikrutko <82881342+dikrutko@users.noreply.github.com>
2022-03-24 20:08:43 +0700

    #

--- src/plugins/route/models.py
+++ src/plugins/route/models.py
@@ -10,6 +10,7 @@ class Route(BaseModel):
     lenght = peewee.DoubleField(index=True)
     height = peewee.DoubleField(index=True)
     hours = peewee.DoubleField(index=True)
+    #link_on_youTube = peewee.CharField()
     color_route = peewee.CharField(index=True)
     area = peewee.ForeignKeyField(Area, backref='routes')
     #points = peewee.ManyToManyField(Point, backref='routes')
